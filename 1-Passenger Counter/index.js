a25856871b621c0d0c533dc0b68acc60141dc94b
celikhilal <118569969+celikhilal@users.noreply.github.com>
2023-01-24 19:42:53 +0300

    update js
    Controlled by giving an id to the buttons.
    The onclick method has been replaced by the addEventListener method.
    The decrement button was disabled and when the increment button was pressed, "disabled" was set to false.
    Set to "disabled" true when count equals zero.

--- 1-Passenger Counter/index.js
+++ 1-Passenger Counter/index.js
@@ -1,26 +1,35 @@
 
 let countEl = document.getElementById("count-el")
 let saveEl = document.getElementById("save-el")
+let decrementBtn = document.getElementById("decrement-btn")
+let saveBtn = document.getElementById("save-btn")
+let resetBtn = document.getElementById("reset-btn")
+let incrementBtn = document.getElementById("increment-btn")
 let count = 0
-function increment(){
-    count ++
-    countEl.textContent = count
-}
 
-function save(){
+incrementBtn.addEventListener("click", function(){
+    count ++
+    decrementBtn.disabled = false
+    countEl.textContent = count
+})
+
+saveBtn.addEventListener("click", function(){
     let countStr = count + " - "
     saveEl.textContent += countStr
     countEl.textContent = 0
-    count = 0    
-}
+    count = 0
+})
 
-function decrement(){
+decrementBtn.addEventListener("click", function(){
     count --
+    if (count === 0){
+        decrementBtn.disabled = true
+    }
     countEl.textContent = count
-}
+})
 
-function reset(){
+resetBtn.addEventListener("click", function(){
     saveEl.textContent = "Previous entires: "
     countEl.textContent = 0
     count = 0 
-}
+})
